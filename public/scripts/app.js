f524e59758175ec9dcbc3968fa7fa51f57a47d31
virginiachae <chae.virginia@gmail.com>
2016-11-30 10:47:11 -0800

    started sprint 4

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,62 +5,27 @@
  */
 
 angular
-  .module('tunely', [])
+  .module('tunely', ['ngRoute'])
+  .config(config)
   .controller('AlbumsIndexController', AlbumsIndexController);
+  .controller('AlbumsShowController', AlbumsShowController);
 
-AlbumsIndexController.$inject = ['$http'];
+  config.$inject = ['$routeProvider', '$locationProvider'];
+  function config(   $routeProvider,  $locationProvider   ) {
+    $routeProvider
+    .when('/', {
+      templateUrl: '/templates/albums',
+      controllerAs: 'albumsIndexCtrl',
+      controller: 'AlbumsIndexController'
+    })
+    .when('/albums/:id', {
+      templateUrl: '/templates/albums-show',
+      controllerAs: 'albumsShowCtrl',
+      controller: 'AlbumsShowController'
+    });
 
-function AlbumsIndexController ($http) {
-  var vm = this;
-  vm.newAlbum = {};
-  vm.newAlbum = {
-    name: 'Viva Hate',
-    artistName: 'Morrissey'
-  };
-
-  $http({
-    method: 'GET',
-    url: '/api/albums'
-  }).then(function successCallback(response) {
-    vm.albums = response.data;
-  }, function errorCallback(response) {
-    console.log('There was an error getting the data', response);
-  });
-
-  vm.createAlbum = function () {
-    $http({
-      method: 'POST',
-      url: '/api/albums',
-      data: vm.newAlbum,
-    }).then(function successCallback(response) {
-      vm.albums.push(response.data);
-    }, function errorCallback(response) {
-      console.log('There was an error posting the data', response);
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
     });
   }
-
-  vm.editAlbum = function (album) {
-    $http({
-      method: 'PUT',
-      url: '/api/albums/'+album._id,
-      data: album
-    }).then(function successCallback(json) {
-      // don't need to do anything!
-    }, function errorCallback(response) {
-      console.log('There was an error editing the data', response);
-    });
-  }
-
-  vm.deleteAlbum = function (album) {
-    $http({
-      method: 'DELETE',
-      url: '/api/albums/'+ album._id
-    }).then(function successCallback(json) {
-      var index = vm.albums.indexOf(album);
-      vm.albums.splice(index,1)
-    }, function errorCallback(response) {
-      console.log('There was an error deleting the data', response);
-    });
-  }
-
-}
