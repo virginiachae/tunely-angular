c7900b26d66622b9c4e9b77f8de449f6542c5fcf
virginiachae <chae.virginia@gmail.com>
2016-11-30 15:43:46 -0800

    finished sprint 5

--- public/scripts/controllers/AlbumsShowController.js
+++ public/scripts/controllers/AlbumsShowController.js
@@ -17,4 +17,43 @@ function AlbumsShowController ($http, $routeParams) {
     console.log('There was an error getting the data', response);
   });
 
-}
+  vm.createSong = function () {
+    $http({
+      method: 'POST',
+      url: '/api/albums/'+$routeParams.id+'/songs/',
+      data: vm.newSong,
+    }).then(function successCallback(response) {
+      vm.album.songs.push(response.data);
+    }, function errorCallback(response) {
+      console.log('There was an error posting the data', response);
+    });
+  }
+
+  vm.deleteSong = function (song) {
+    $http({
+      method: 'DELETE',
+      url: '/api/albums/'+$routeParams.id+'/songs/' +song._id,
+    }).then(function successCallback(json) {
+      var index = vm.album.songs.indexOf(song);
+      vm.album.songs.splice(index,1)
+    }, function errorCallback(response) {
+      console.log('There was an error deleting the data', response);
+    });
+  }
+
+  vm.editSong = function (song) {
+    $http({
+      method: 'PUT',
+      url: '/api/albums/'+$routeParams.id+'/songs/' +song._id,
+      data: song
+    }).then(function successCallback(json) {
+      // don't need to do anything!
+    }, function errorCallback(response) {
+      console.log('There was an error editing the data', response);
+    });
+  }
+
+
+
+
+} //last curly bracket
